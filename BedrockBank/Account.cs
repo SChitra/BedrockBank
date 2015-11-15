0e19fbc8e55efefc01499d9d4e6e736c733dc7ac
C S <sethuchitra@live.com>
2015-11-15 11:06:36 -0800

    Initial code for check in

---
+++ BedrockBank/Account.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BedrockBank
+{
+    /// <summary>
+    /// This class provides information for 
+    /// a Bedrock account
+    /// </summary>
+    class Account
+
+    {
+        #region Properties
+        /// <summary>
+        /// Name of the account holder
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Account number for the account
+        /// </summary>
+        public int AccountNumber { get; set; }
+        /// <summary>
+        /// 
+        /// Balance in account
+        /// </summary>
+        
+        public decimal Balance { get; set; }
+        #endregion
+
+
+        #region Methods
+
+        #endregion
+
+
+
+
+
+
+    }
+}
