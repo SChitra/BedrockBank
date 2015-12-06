a0e66c23365203ade53339a71268d5175a4590cf
C S <sethuchitra@live.com>
2015-12-06 11:56:02 -0800

    Overloads and static classes

--- BedrockBank/Account.cs
+++ BedrockBank/Account.cs
@@ -10,26 +10,54 @@ namespace BedrockBank
     /// This class provides information for 
     /// a Bedrock account
     /// </summary>
-    class Account
+    public class Account
 
     {
+        #region Variables
+        /// <summary>
+        /// Shared memeory for storing the last account number
+        /// </summary>
+        private static int lastAccountNumber = 0;
+
+
+        #endregion
         #region Properties
         /// <summary>
         /// Name of the account holder
         /// </summary>
         public string Name { get; set; }
         /// <summary>
-        /// Account number for the account
+        /// Account number for the account. Private for set so nobody can change it
         /// </summary>
-        public int AccountNumber { get; set; }
+        public int AccountNumber { get; private set; }
         /// <summary>
         /// 
         /// Balance in account
         /// </summary>
         
-        public decimal Balance { get; set; }
+        public decimal Balance { get; private set; }
+
+        public int SSN { get; set; }
         #endregion
 
+        #region Constructors
+        //Default Constructor
+        public Account()
+        {
+           AccountNumber = ++lastAccountNumber;
+        }
+
+        public Account(string name):this()
+        {
+            this.Name = name;
+        }
+        public Account(string name, decimal amount):this(name)
+        {
+            //Name = name;
+            Deposit(amount);
+        }
+
+        #endregion
 
         #region Methods
         /// <summary>
@@ -44,9 +72,22 @@ namespace BedrockBank
             return Balance;
 
         }
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="amount"></param>
+        /// <returns></returns>
+        /// <exception></exception>
         public decimal Withdrawal(decimal amount)
         {
+            if(amount <0)
+            {
+                throw new ArgumentNullException("Balance should be atleaset 1$");
+            }
+            if(amount > Balance)
+            {
+                throw new ArgumentException("Insufficient funds");
+            }
             Balance -= amount;
             return Balance;
         }
--- BedrockBank/Program.cs
+++ BedrockBank/Program.cs
@@ -9,13 +9,31 @@ namespace BedrockBank
     class Program
     {
         static void Main(string[] args)
+
         {
+
+            var account = BankFactory.CreateAccount("Chitra Checking", 500);
             //Create an instance of an account == creating an object
-            var account = new Account();
-            account.AccountNumber = 123;
-            account.Name = "Chitra Checking";
-            account.Deposit(500);
-            Console.WriteLine(account.Balance);
+            //var account = new Account();
+            ////account.AccountNumber = 123;
+            //account.Name = "Chitra Checking";
+            //account.Deposit(500);
+            Console.WriteLine("Name: {0},Account Number: {1}, Balance: {2:c}",account.Name,account.AccountNumber,account.Balance);
+
+            var savingsAccount = BankFactory.CreateAccount("CSethu", 10);
+            //var savingsAccount = new Account();
+            //savingsAccount.Name = "CSethu";
+            //savingsAccount.Deposit(10);
+            //Exception handling
+            try
+            {
+                savingsAccount.Withdrawal(100);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("Ooops something went wrong {0}", ex.Message);
+            }
+            Console.WriteLine("Name: {0},Account Number: {1}, Balance: {2:c}", savingsAccount.Name, savingsAccount.AccountNumber, savingsAccount.Balance);
         }
     }
 }
